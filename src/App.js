e0c0d600d812166093e8a7ab9b12044849693ccf
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-08 18:00:14 +0530

    trend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './Pages/Home'
 import Development from './Pages/Development'
+import Trends from './Pages/Trends'
 
 const App = () => {
   return (
     <div>
       <Home />
       <Development />
+      <Trends />
     </div>
   )
 }
