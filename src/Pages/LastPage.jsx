e42ed83123fcef75407094c38b45322bca84f4c7
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-12 13:32:10 +0530

    some updates

--- src/Pages/LastPage.jsx
+++ src/Pages/LastPage.jsx
@@ -6,10 +6,12 @@ const LastPage = () => {
             <div id='last-left'>
                 <img src='https://s3-alpha-sig.figma.com/img/fbf0/510c/d7292545bf7da63d70375df2f8e28d12?Expires=1692576000&Signature=AjWhxvvqrZBchzHBwJ06yO~1-jubHD3ltXbxywNCCiTX3cusZMnyOW5AggiyEbkNkjNYMrCFGtQ9BRgtgDDFa6XdmiIHEg0UtiG7wRGmxNLPQlwsEiUUxLhDpXZvBewKEOYTHxgl7tye4JScsywIvpMrjYMiK-w~KT7IE0zkqMZ1TdeJhmPFjxcz~4NDAocbqEwJte051XF1yl5PZ-qk~fM20iJo5FIqr7gc-nbfKY4b3AW4D-z4JWNpEsa0AeLj-kvIONU2uRWe2B8iSyH5BSzacEbVjCYxczeqHwrOXaznu7ODejzMahNnPPl8uKkc6X1nY2pyhuz08AGUGmBJ1A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' alt='last'></img>
                 <p>Jorem ipsum dolor sit amet, consectetur <br></br>adipiscing elit. Nunc vulputate libero et<br></br> velit interdum, ac aliquet odio mattis.<br></br> Class aptent taciti sociosqu ad litora<br></br> torquent per conubia nostra, per inceptos <br></br>himenaeos. Curabitur tempus urna at<br></br> turpis condimentum lobortis.Jorem<br></br> ipsum dolor sit amet, consectetur<br></br> adipiscing elit. Nunc vulputate libero et <br></br>velit interdum, ac </p>
-                <input id='last-input' placeholder='Enter Your Email'></input>
-                <div id='input-start'>Get Started<svg xmlns="http://www.w3.org/2000/svg" width="33" height="34" viewBox="0 0 33 34" fill="none">
-                    <path d="M5.48368 26.292C5.17494 25.94 5.02057 25.5501 5.02057 25.1222C5.02057 24.6943 5.17494 24.3216 5.48368 24.0038L12.2063 16.9001L5.44416 9.78635C5.13542 9.49631 4.98763 9.1166 5.00081 8.64721C5.01398 8.17785 5.17494 7.79483 5.48368 7.49814C5.78613 7.15274 6.14099 6.98696 6.54828 7.0008C6.95557 7.01464 7.31044 7.18042 7.61289 7.49814L15.5084 15.7925C15.6465 15.9375 15.7581 16.1066 15.8435 16.2998C15.9288 16.493 15.9715 16.6931 15.9715 16.9001C15.9715 17.1347 15.9288 17.347 15.8435 17.5369C15.7581 17.7267 15.6465 17.8942 15.5084 18.0392L7.65241 26.292C7.34997 26.6098 6.98851 26.7617 6.56805 26.7479C6.14758 26.734 5.78613 26.5821 5.48368 26.292Z" fill="white" />
-                </svg></div>
+                <div id='box-input'>
+                    <input id='last-input' placeholder='Enter Your Email'></input>
+                    <div id='input-start'>Get Started<svg xmlns="http://www.w3.org/2000/svg" width="33" height="34" viewBox="0 0 33 34" fill="none">
+                        <path d="M5.48368 26.292C5.17494 25.94 5.02057 25.5501 5.02057 25.1222C5.02057 24.6943 5.17494 24.3216 5.48368 24.0038L12.2063 16.9001L5.44416 9.78635C5.13542 9.49631 4.98763 9.1166 5.00081 8.64721C5.01398 8.17785 5.17494 7.79483 5.48368 7.49814C5.78613 7.15274 6.14099 6.98696 6.54828 7.0008C6.95557 7.01464 7.31044 7.18042 7.61289 7.49814L15.5084 15.7925C15.6465 15.9375 15.7581 16.1066 15.8435 16.2998C15.9288 16.493 15.9715 16.6931 15.9715 16.9001C15.9715 17.1347 15.9288 17.347 15.8435 17.5369C15.7581 17.7267 15.6465 17.8942 15.5084 18.0392L7.65241 26.292C7.34997 26.6098 6.98851 26.7617 6.56805 26.7479C6.14758 26.734 5.78613 26.5821 5.48368 26.292Z" fill="white" />
+                    </svg></div>
+                </div>
             </div>
 
             <div id='last-right'>
@@ -47,7 +49,17 @@ const LastPage = () => {
 
                     </ul>
                 </div>
+                <div id='last-contact'>
+                    <div>CONTACT :<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                        <path d="M22.8063 23.9699C22.9892 23.9699 23.1812 23.9652 23.3823 23.9557C23.5835 23.9461 23.7755 23.9326 23.9583 23.9151V17.4967L18.4726 16.3996L15.2908 19.5813C13.9011 18.7402 12.6255 17.7436 11.464 16.5916C10.3025 15.4396 9.33807 14.2053 8.57079 12.8887L11.6977 9.73438L10.6828 4.22119H4.26447C4.2279 4.40405 4.20961 4.59605 4.20961 4.7972V5.3732C4.20961 7.73208 4.73515 10.0317 5.78622 12.2721C6.83729 14.5125 8.22702 16.5009 9.9554 18.2373C11.6838 19.9745 13.6678 21.3642 15.9074 22.4065C18.1471 23.4488 20.4467 23.9699 22.8063 23.9699Z" fill="#0267FF" />
+                    </svg>+91  9876 543 21  </div>
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                        <path d="M22.8615 5.30078H5.30704C4.10017 5.30078 3.11273 6.28822 3.11273 7.49508V20.6609C3.11273 21.8678 4.10017 22.8552 5.30704 22.8552H22.8615C24.0683 22.8552 25.0558 21.8678 25.0558 20.6609V7.49508C25.0558 6.28822 24.0683 5.30078 22.8615 5.30078ZM22.4226 9.96368L15.2472 14.451C14.5341 14.9009 13.6344 14.9009 12.9213 14.451L5.7459 9.96368C5.63588 9.90192 5.53954 9.81848 5.46271 9.71841C5.38587 9.61834 5.33014 9.50373 5.29889 9.38149C5.26763 9.25926 5.26151 9.13196 5.28088 9.0073C5.30024 8.88263 5.34471 8.76319 5.41157 8.6562C5.47844 8.54922 5.56632 8.45691 5.66989 8.38487C5.77347 8.31283 5.89058 8.26255 6.01414 8.23709C6.13771 8.21162 6.26516 8.21149 6.38878 8.2367C6.51239 8.26192 6.62961 8.31195 6.73333 8.38378L14.0842 12.9808L21.4352 8.38378C21.5389 8.31195 21.6561 8.26192 21.7797 8.2367C21.9033 8.21149 22.0308 8.21162 22.1544 8.23709C22.2779 8.26255 22.395 8.31283 22.4986 8.38487C22.6022 8.45691 22.6901 8.54922 22.7569 8.6562C22.8238 8.76319 22.8683 8.88263 22.8876 9.0073C22.907 9.13196 22.9009 9.25926 22.8696 9.38149C22.8384 9.50373 22.7826 9.61834 22.7058 9.71841C22.629 9.81848 22.5326 9.90192 22.4226 9.96368Z" fill="#0267FF" />
+                    </svg> reach@fxdx.com</div>
+                </div>
+
             </div>
+
 
         </div>
     )
